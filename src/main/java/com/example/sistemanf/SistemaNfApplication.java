ecc9cc1eb4666177c749fd0aedd4026bffec7c20
Pedro Quessada <pedroluiz.quessada@gmail.com>
2025-07-11 13:01:39 -0300

    build: criando o projeto

---
+++ src/main/java/com/example/sistemanf/SistemaNfApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemanf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaNfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaNfApplication.class, args);
+    }
+
+}
